8ca702ca9fa35ac69e555a8d0f82964dfa51ba2c
David Brooks <d.brooks@auckland.ac.nz>
2025-02-21 10:23:47 +1300

    Save WIP on BG model creation tool.

--- tools/model2json.py
+++ tools/model2json.py
@@ -4,6 +4,8 @@ import pathlib
 from typing import Optional
 from urllib.parse import urlparse
 
+#===================
+
 import rdflib
 
 #===================
@@ -43,6 +45,25 @@ class Prefixer:
 
 #===================
 
+type QueryResult = dict[str, str]
+
+def sparql_query(graph: rdflib.Graph, prefixer: Prefixer, sparql: str, keys: list[str], trace=False) -> list[QueryResult]:
+    sparql = f'{prefixer.sparql_prefixes}\n{sparql}'
+    if trace:
+        print(sparql)
+        print()
+        print(', '.join(keys))
+    query_results: list[QueryResult] = []
+    for row in graph.query(sparql):
+        row_dict = row.asdict()      # type: ignore
+        result = {key: prefixer.curie(row_dict[key]) for key in keys}
+        query_results.append(result)
+        if trace:
+            print(', '.join(result.values()))
+    return query_results
+
+#===================
+
 class Template:
 
     def instantiate(self, ):
@@ -62,45 +83,85 @@ PREFIX_DICT: PrefixDict = {
 
 #===================
 
-def query_rdf(graph: rdflib.Graph, prefixer: Prefixer, sparql: str, keys: list[str]):
-    print(', '.join(keys))
-    for row in graph.query(f'{prefixer.sparql_prefixes}\n{sparql}'):
-        result_dict = row.asdict()      # type: ignore
-        print(', '.join([f'"{prefixer.curie(result_dict[key])}"' for key in keys]))
-
-#===================
-
-class Model:
+class Models:
     def __init__(self, model_path: str, prefix_dict: PrefixDict):
         self.__graph = rdflib.Graph()
         self.__graph.parse(model_path, format='turtle')
         self.__prefixer = Prefixer(prefix_dict)
         self.__prefixer.add_uri(':', f'{make_uri(model_path)}#')
+        query_results = sparql_query(self.__graph, self.__prefixer, f'''
+SELECT ?model ?component ?template ?port ?node
+{{
+    ?model
+        a bg:Model ;
+        bg:component ?component .
+    ?component
+        tpl:template ?template ;
+        tpl:connection [
+            tpl:port ?port ;
+            bg:node ?node
+        ]
+}} ORDER BY ?model ?component ?tpl ?port ?node
+''', ['model', 'component', 'template', 'port', 'node'])
+        model_component_ports = []
+        current_model = {}
+        last_component = None
+        for result_row in query_results:
+            if result_row['model'] != current_model.get('model'):
+                if len(current_model) and len(current_model['components']):
+                    model_component_ports.append(current_model)
+                current_model = {
+                    'model': result_row['model'],
+                    'components': []
+                }
+                last_component = None
+            if last_component is not None and result_row['component'] != last_component.id:
+                current_model['components'].append(last_component.as_dict())
+                last_component = None
+            if last_component is None:
+                last_component = Component(result_row['component'], result_row['template'])
+            last_component.add_port(result_row['port'], result_row['node'])
+        if len(current_model):
+            if last_component is not None:
+                current_model['components'].append(last_component.as_dict())
+            if len(current_model['components']):
+                model_component_ports.append(current_model)
+        self.__model_component_ports = model_component_ports
 
-    def query(self, sparql: str, keys: list[str]):
-        print(', '.join(keys))
-        for row in self.__graph.query(f'{self.__prefixer.sparql_prefixes}\n{sparql}'):
-            result_dict = row.asdict()      # type: ignore
-            print(', '.join([f'"{self.__prefixer.curie(result_dict[key])}"' for key in keys]))
+    @property
+    def model_component_ports(self):
+        return self.__model_component_ports
+
+#===================
+
+class Component:
+    def __init__(self, id: str, template: str):
+        self.__id = id
+        self.__ports = []
+        self.__template = template
+
+    @property
+    def id(self):
+        return self.__id
+
+    def add_port(self, port: str, node: str):
+        self.__ports.append({
+            'port': port,
+            'node': node
+        })
+
+    def as_dict(self):
+        return {
+            'template': self.__template,
+            'ports': self.__ports
+        }
 
 #===================
 
 def main(model_path: str):
 #=========================
-
-    model = Model(model_path, PREFIX_DICT)
-    model.query(f'''
-SELECT ?model ?tpl ?conn ?port ?node
-{{
-    ?model a bg:Model .
-    ?model bg:component ?component .
-    ?component tpl:template ?tpl .
-    ?component tpl:connection ?conn .
-    ?conn tpl:port ?port .
-    ?comm bg:node ?node .
-}} ORDER BY ?model ?tpl ?node
-''', ['model', 'tpl', 'conn', 'port', 'node'])
-
+    model = Models(model_path, PREFIX_DICT)
+    print(json.dumps(model.model_component_ports, indent=4))
 
 #===================
 
